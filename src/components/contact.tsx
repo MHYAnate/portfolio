c909752eb6c496fc066208710a87fbf8acf6583a
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-03-18 14:04:48 +0100

    feat:linkedin

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -64,7 +64,7 @@ export default function ContactComponent() {
 					<span className="text-sm font-medium">GitHub</span>
 				</a>
 				<a
-					href="https://www.linkedin.com/in/mohammed-hayatudeen-yusuf-04804a299"
+					href="www.linkedin.com/in/mohammed-hayatudeen-yusuf-anate-491113357"
 					target="_blank"
 					rel="noopener noreferrer"
 					className="flex flex-col items-center space-y-2 group"
